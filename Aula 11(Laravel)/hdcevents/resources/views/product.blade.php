c881d484c953dab3ad3fe65bb82ebb0b2bf607af
Juan <juanaleixoferes@hotmail.com>
2022-07-06 01:55:44 -0300

    Clean

--- Aula 11(Laravel)/hdcevents/resources/views/product.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Produto')
-
-@section('content')
-
-<h1>Exibindo o produto: {{$id}}</h1>
-
-@endsection
--- README.md
+++
@@ -1,3 +0,0 @@
-# DW2A5
-
-Arquivos atualizados e Páginas disponíveis para acesso: http://177.84.63.83/DW2/
